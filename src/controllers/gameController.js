d31d4036c4fd8c5473a32b51695acba596844656
Mugsy Carter <mugsycarter@gmail.com>
2016-12-01 14:59:07 -0800

    more fight functionality added.

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -41,7 +41,7 @@ export default function gameController($scope){ //eslint-disable-line no-unused-
   this.fight = function(){
     console.log('player clicked fight');
     if (rooms[player.room].monster != null){
-      console.log('player is fighting this monster: ', rooms[player.room].monster);
+      console.log('player is fighting this monster: '+ rooms[player.room].monster+ ' with a '+player.item);
     }
     else {
       alert('There is nothing in this room to fight.');
