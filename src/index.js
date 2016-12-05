72fd32fbd0362d37b52d679314b55ffb29976635
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-12-05 14:19:38 -0800

    Made index.js root of application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import angular from 'angular';
 import globalController from './controllers/globalController';
 import gameController from './controllers/gameController';
-import './main.css';
 import GameSpace from './models/game-space';
 import Player from './models/player';
+import './main.css';
 
 angular.module('orcSlayer', [])
   .controller('gameController', gameController)
